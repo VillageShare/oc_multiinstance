cc6631cd567c817b592dd19939bda77e5af3e9cd
Sarah Jones <sarah.e.p.jones@gmail.com>
2013-04-20 15:22:50 -0700

    missing this

--- core/crontask.php
+++ core/crontask.php
@@ -166,7 +166,7 @@ class CronTask {
 		foreach ($dirs as $dir){
 			$files = $this->api->glob($dir . "/a*");
 			//Only non-central server should process responses
-			if ($api->getSystemValue('centralServer') !== $api->getSystemValue('location')) {
+			if ($this->api->getSystemValue('centralServer') !== $api->getSystemValue('location')) {
 				$files2 = $this->api->glob($dir . "/r*");
 				$files = array_merge($files, $files2);
 			}
