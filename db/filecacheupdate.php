96ba5c674949233929bc44ab43718e1edc809efb
Sarah Jones <sarah.e.p.jones@gmail.com>
2013-05-04 14:57:18 -0700

    Need filecacheupdates

---
+++ db/filecacheupdate.php
@@ -0,0 +1,44 @@
+<?php
+/**
+* ownCloud - App Template Example
+*
+* @author Bernhard Posselt
+* @copyright 2012 Bernhard Posselt nukeawhale@gmail.com
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+* License as published by the Free Software Foundation; either
+* version 3 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+*
+* You should have received a copy of the GNU Affero General Public
+* License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+namespace OCA\MultiInstance\Db;
+
+
+use \OCA\AppFramework\Db\Entity;
+
+
+class FileCacheUpdate extends Entity {
+
+	public $fileid;
+	public $updatedAt;
+	public $state;
+
+	const VALID = 1;
+	const DELETED = 0;
+
+	public function __construct($fileid, $updatedAt, $state){
+		$this->setFileid($fileid);
+		$this->setUpdatedAt($updatedAt);
+		$this->setState($state);
+	}
+
+}
---
+++ db/filecacheupdatemapper.php
@@ -0,0 +1,98 @@
+<?php
+/**
+* ownCloud - App Template Example
+*
+* @author Bernhard Posselt
+* @copyright 2012 Bernhard Posselt nukeawhale@gmail.com
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+* License as published by the Free Software Foundation; either
+* version 3 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+*
+* You should have received a copy of the GNU Affero General Public
+* License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+namespace OCA\MultiInstance\Db;
+
+use \OCA\AppFramework\Core\API;
+use \OCA\AppFramework\Db\Mapper;
+use \OCA\AppFramework\Db\Entity;
+use \OCA\AppFramework\Db\DoesNotExistException;
+use \OCA\AppFramework\Db\MultipleObjectsReturnedException;
+
+use \OCA\MultiInstance\Db\FilecacheUpdate;
+
+class FilecacheUpdateMapper extends Mapper {
+
+
+
+	/**
+	 * @param API $api: Instance of the API abstraction layer
+	 */
+	public function __construct($api){
+		parent::__construct($api, 'multiinstance_filecache_updates');
+
+	}
+
+	/**
+	 * Finds an item by id
+	 * @throws DoesNotExistException: if the item does not exist
+	 * @return the item
+	 */
+	public function find($fileid){
+		$sql = 'SELECT * FROM `' . $this->getTableName() . '` WHERE `fileid` = ?';
+		$params = array($fileid);
+
+		$result = array();
+		
+		$result = $this->execute($sql, $params);
+		$row = $result->fetchRow();
+
+		if ($row === false) {
+			throw new DoesNotExistException("FilecacheUpdate with fileid {$fileid} does not exist!");
+		} elseif($result->fetchRow() !== false) {
+			throw new MultipleObjectsReturnedException("FilecacheUpdate with fileid {$fileid} returned more than one result.");
+		}
+		return new FilecacheUpdate($row);
+
+	}
+
+	public function exists($fileid){
+		try{
+			$this->find($fileid);
+		}
+		catch (DoesNotExistException $e){
+			return false;
+		}
+		catch (MultipleObjectsReturnedException $e){
+			return true;
+		}
+		return true;
+	}
+
+	/**
+	 * Finds all Items
+	 * @return array containing all items
+	 */
+	public function findAll(){
+		$result = $this->findAllQuery($this->getTableName());
+
+		$entityList = array();
+		while($row = $result->fetchRow()){
+			$entity = new FilecacheUpdate($row);
+			array_push($entityList, $entity);
+		}
+
+		return $entityList;
+	}
+
+
+}
